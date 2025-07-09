a7a0135425afd68ccbfa6e46213cca37ce23e7e0
sharath.kyoka <sharath.d.bca@gmail.com>
2025-07-09 09:46:55 +0530

    Add files via upload

---
+++ grade score.py
@@ -0,0 +1,8 @@
+score = int(input("enter your score: "))
+
+if score >= 90:
+    print("Grade: A")
+if score >= 80 and score < 90:
+    print("Grade: B")
+if score < 80:
+    print("Grade: C")
---
+++ tempcheck.py
@@ -0,0 +1,8 @@
+temp = int(input("temperature: "))
+
+if temp > 30:
+    print("its hot.")
+elif temp > 20:
+    print("great weather.")
+else:
+    print("its cold.")
